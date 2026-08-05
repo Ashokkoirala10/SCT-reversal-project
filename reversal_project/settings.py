1af5ac7fd61f9da20184db802d9dd48538809ede
Ashok AK. Koirala <ashok261@sct.org>
2026-08-05 16:11:51 +0545

    updated database

--- reversal_project/settings.py
+++ reversal_project/settings.py
@@ -56,13 +56,22 @@ WSGI_APPLICATION = "reversal_project.wsgi.application"
 
 # --- Database -------------------------------------------------------------
 # Simple sqlite db is enough for the audit log.
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "reversal_db",
+        "USER": "postgres",
+        "PASSWORD": "ashok",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
